8c8de5e5822b6f496bf9e4aefc0bb4815581c3e9
Rexxxx123 <66236838+Rexxxx123@users.noreply.github.com>
2020-06-09 00:14:58 -0400

    'Demo'

---
+++ Chapter_1/Ch1_D1.py
@@ -0,0 +1,23 @@
+print(1 + 1)
+print(2 + 3 * 6)
+print((2 + 3) * 6)
+print(48565878 * 578453)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(23 % 7)
+print((5 - 1)*((7 + 1) / (3 - 1)))
+print('Alice' + 'Bob')
+print('Alice' * 5)
+spam=40
+print(spam)
+eggs=2
+print(spam + eggs)
+print(spam + eggs + spam)
+spam = spam + 2
+print(spam)
+spam = 'Hello'
+print(spam)
+spam = 'GB'
+print(spam)
+
