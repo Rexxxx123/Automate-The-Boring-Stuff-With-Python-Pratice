468972b8890a4976123dcda799fec02ffff5933f
Rexxxx123 <66236838+Rexxxx123@users.noreply.github.com>
2020-06-30 21:52:52 -0400

    'Pratice'

--- Chapter_7/P2_Regex_Version_of_strip().py
+++ Chapter_7/P2_Regex_Version_of_strip().py
@@ -4,16 +4,16 @@ def stripRegex(s,character=None):
         character = '\s'
     return re.sub(r'^[{0}]+|[{0}]+$'.format(character), '', s)
 
-
+# use example form Chapter 6
 x1 = ''
 x2 = 'ampS'
 x3 = 'mapS'
 x4 = 'Spam'
-string1 = '      Hello world   '
+string1 = '     Hello world   '
 string2 = 'SpamSpamBaconSpamEggsSpamSpam'
 
-print(stripRegex(string1))  # 'Hello world!!!'
-print(stripRegex(string1, x1))  # '      Hello world!!!   '
+print(stripRegex(string1))  # 'Hello world'
+print(stripRegex(string1, x1))  # '      Hello world   '
 print(stripRegex(string2, x2))  # 'BaconSpamEggs'
 print(stripRegex(string2, x3))  # 'BaconSpamEggs'
 print(stripRegex(string2, x4))  # 'BaconSpamEggs'
